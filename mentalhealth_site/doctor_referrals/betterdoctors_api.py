10c6fe67629e7fd4aa1d1661652176d138c1c8cc
Shi Zhang <slz250@nyu.edu>
2017-11-05 00:49:32 -0400

    done with doctor referrals

--- mentalhealth_site/doctor_referrals/betterdoctors_api.py
+++ mentalhealth_site/doctor_referrals/betterdoctors_api.py
@@ -1,8 +1,10 @@
 #api demo
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
+from urllib2 import urlopen
 import json
 import geocoder
+from random import randint
 
 """
 &location=42.3912%2C-72.5267%2C100&user_location=42.3912%2C-72.5267&skip=0&limit=10&user_key=349beaed7c0fbb0a3384f8e45209205a
@@ -11,8 +13,8 @@ def betterdoctor_search(latlng, apiKey):
     url = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=psychologist&user_key=" + apiKey
     latlngUrl = str(latlng[0]) + "%2C" + str(latlng[1])
     locationUrl = "&location=" + latlngUrl + "%2C100&user_location=" + latlngUrl
-    finalUrl = url + locationUrl + "&skip=0&limit=10"
-    print(finalUrl)
+    skip = str(randint(0, 38) * 5)
+    finalUrl = url + locationUrl + "&skip=" + skip + "&limit=5"
     json_obj = urlopen(finalUrl)
     data = json.load(json_obj)
     dataJSON = data["data"]
@@ -27,7 +29,6 @@ def betterdoctor_search(latlng, apiKey):
             "lname": lname,
             "bio": bio
         })
-        # print(fname + " " + lname + "\n" + bio + "\n\n\n")
     return storage
 
 def betterdoctor_searchDriver():
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shizhang/nyu_cs_work/hackathons/hackumass/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
